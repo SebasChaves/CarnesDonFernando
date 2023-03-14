55c53595decfff4d2ec52812af8fa50237a61ae2
sebas <sebas.chacon.e@gmail.com>
2023-03-13 19:04:42 -0600

    Avance uno

---
+++ CarnesDonFernando/BackEnd/Models/IngredienteModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BackEnd.Models
+{
+    public class IngredienteModel
+    {
+
+        public int IdIngrediente { get; set; }
+        public string Descripcion { get; set; } = null!;
+        //public int IdReceta { get; set; }
+
+    }
+}
